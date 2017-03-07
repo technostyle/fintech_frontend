450a7fcee4a3b03f3864e5470ad183400a8f0c0e
Perloff Danila <perloff.danila@gmail.com>
2017-03-07 13:19:49 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var first;
+var first = prompt();
+var second = prompt();
 
-alert('etner something');
-first = prompt();
-alert(first);
-alert('Скрипт работает');
+
+function is_anagram(first, second){
+
+	var first_length = first.length
+	if (first_length != second.length)
+		return false
+
+	var str = second;
+	var cur, index
+	for (var i = 0; i < first_length; i++)
+	{
+		cur = first.charAt(i);
+		index = str.indexOf(cur);
+		if (index == -1)
+			return false;
+		str = str.substring(0, index) + str.substring(index + 1, first_length);
+	}
+
+	return true;
+}
+
+alert(is_anagram(first, second));
